702c24651eff98d3740a7e4b7930a8fc498207c5
Asn-lwak <asncrafter21032009@gmail.com>
2024-11-12 15:03:14 +0630

    Update welcome.js

--- js/welcome.js
+++ js/welcome.js
@@ -1,12 +1,8 @@
 document.addEventListener("DOMContentLoaded", function () {
     const playButton = document.getElementById("play-button");
-
-    playButton.addEventListener("click", function () {
-        const playerName = document.getElementById("player-name").value;
-        if (playerName.trim() === "") {
-            alert("Please enter your name.");
-        } else {
+    if (playButton) {
+        playButton.addEventListener("click", function () {
             window.location.href = "mode-selection.html";
-        }
-    });
+        });
+    }
 });
