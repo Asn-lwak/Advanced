fdb00ef36fa875ccd8970c661edfb31eef7c2e8e
Asn-lwak <asncrafter21032009@gmail.com>
2024-11-12 15:51:24 +0630

    Update difficulty-selection.js

--- js/difficulty-selection.js
+++ js/difficulty-selection.js
@@ -1,14 +1,9 @@
 document.addEventListener("DOMContentLoaded", function () {
     const easyButton = document.getElementById("easy-button");
-    const mediumButton = document.getElementById("medium-button");
     const hardButton = document.getElementById("hard-button");
 
     easyButton.addEventListener("click", function () {
         window.location.href = "game.html?difficulty=easy";
-    });
-
-    mediumButton.addEventListener("click", function () {
-        window.location.href = "game.html?difficulty=medium";
     });
 
     hardButton.addEventListener("click", function () {
