39e2416b05167f98c2f63f2954c962221f6c6484
Asn-lwak <asncrafter21032009@gmail.com>
2024-11-13 12:12:07 +0630

    Update mode-selection.js
    Back button

--- js/mode-selection.js
+++ js/mode-selection.js
@@ -1,6 +1,7 @@
 document.addEventListener("DOMContentLoaded", function () {
     const singlePlayerButton = document.getElementById("single-player-button");
     const twoPlayerButton = document.getElementById("two-player-button");
+    const backButton = document.getElementById("back-button");
 
     singlePlayerButton.addEventListener("click", function () {
         window.location.href = "difficulty-selection.html";
@@ -8,5 +9,9 @@ document.addEventListener("DOMContentLoaded", function () {
 
     twoPlayerButton.addEventListener("click", function () {
         window.location.href = "game.html";
+    });
+
+    backButton.addEventListener("click", function () {
+        window.location.href = "welcome.html";
     });
 });
